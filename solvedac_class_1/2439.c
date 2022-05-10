5381a6d86e8a386e7916fb0caa2446be43b2c96b
raehy19 <69339099+raehy19@users.noreply.github.com>
2022-05-10 16:44:43 +0900

    2439

---
+++ solvedac_class_1/2439.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int size;
+
+	scanf("%d", &size);
+	for (int i = 0; i < size; i++)
+	{
+		if (i != 0)
+			printf("\n");
+		for (int j = size - i - 1; j > 0; j--)
+			printf(" ");
+		for (int j = 0; j < i + 1; j++)
+			printf("*");
+	}
+	return 0;
+}
