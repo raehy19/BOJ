3bccb1b6549b1e73e7f6663ff4047b15bd814cf3
raehy19 <69339099+raehy19@users.noreply.github.com>
2022-05-12 20:12:56 +0900

    1181

--- solvedac_class_2/1181.c
+++ solvedac_class_2/1181.c
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-void	ft_swap(char **str1, char **str2)
+int	ft_compare(const void *a, const void *b)
 {
-	char	*temp;
+	char *str1 = *((char **)a);
+	char *str2 = *((char **)b);
 
-	temp = *str1;
-	*str1 = *str2;
-	*str2 = temp;
+	if (strlen(str1) > strlen(str2))
+		return 1;
+	else if (strlen(str1) < strlen(str2))
+		return -1;
+	else if (strcmp(str1, str2) > 0)
+		return 1;
+	else if (strcmp(str1, str2) < 0)
+		return -1;
+	else
+		return 0;
 }
 
 int main()
@@ -23,16 +31,7 @@ int main()
 		*(words + i) = (char *) malloc(sizeof(char) * 52);
 		scanf("%s", *(words + i));
 	}
-	for (int i = 0; i < size - 1; i++)
-	{
-		for (int j = 0; j < size - 1 - i; j++)
-		{
-			if ((strlen(*(words + j)) > strlen(*(words + j + 1)))
-				|| ((strlen(*(words + j)) == strlen(*(words + j + 1)))
-					&& (strcmp(*(words + j), *(words + j + 1)) > 0)))
-				ft_swap((words + j), (words + j + 1));
-		}
-	}
+	qsort(words, size, sizeof(char *), ft_compare);
 	printf("%s", *(words));
 	for (int i = 1; i < size; i++)
 	{
