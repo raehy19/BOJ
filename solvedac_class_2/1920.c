72ab66b3401d0645f9bff569ef6f336e731f48f7
raehy19 <69339099+raehy19@users.noreply.github.com>
2022-05-15 16:26:57 +0900

    1920

--- solvedac_class_2/1920.c
+++ solvedac_class_2/1920.c
@@ -1,37 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int	ft_compare(const void *a, const void *b)
+{
+	if (*((int *)a) > *((int *)b))
+		return 1;
+	else if (*((int *)a) < *((int *)b))
+		return -1;
+	else
+		return 0;
+}
+
 int main()
 {
-	int	n, m, flag;
+	int	n, m, temp, min, max, median;
 	int	*nums;
-	int *to_search;
+	int *result;
 
 	scanf("%d", &n);
 	nums = (int *) malloc(sizeof(int) * n);
 	for (int i = 0; i < n; ++i)
 		scanf("%d", nums + i);
+	qsort(nums, n, sizeof(int), ft_compare);
 	scanf("%d", &m);
-	to_search = (int *) malloc(sizeof(int) * m);
+	result = (int *) malloc(sizeof(int) * m);
 	for (int i = 0; i < m; ++i)
 	{
-		scanf("%d", to_search + i);
-		flag = 0;
-		for (int j = 0; j < n; ++j)
+		scanf("%d", &temp);
+		min = 0;
+		max = n -1;
+		while (min <= max)
 		{
-			if (*(nums + j) == *(to_search + i))
-			{
-				flag = 1;
-				break ;
-			}
+			median =((max + min) / 2);
+			if (*(nums + median) > temp)
+				max = median - 1;
+			else if (*(nums + median) < temp)
+				min = median + 1;
+			else
+				break;
 		}
-		*(to_search + i) = flag;
+		if (temp == *(nums + median))
+			*(result + i) = 1;
+		else
+			*(result + i) = 0;
 	}
 	for (int i = 0; i < m; ++i)
 	{
 		if (i != 0)
 			printf("\n");
-		printf("%d",*(to_search + i));
+		printf("%d",*(result + i));
 	}
+	free(nums);
+	free(result);
 	return 0;
 }
