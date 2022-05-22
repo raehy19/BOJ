10c5759216822d6d8aee4749a3e558e19f573a87
raehy19 <69339099+raehy19@users.noreply.github.com>
2022-05-22 09:32:50 +0900

    15829

--- solvedac_class_2/15829.c
+++ solvedac_class_2/15829.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_power(int nb, int power)
-{
-	int	result;
-
-	result = 1;
-	while (power > 0)
-	{
-		result = result * nb;
-		power--;
-	}
-	return (result);
-}
-
 int main()
 {
-	int			l;
-	long long	hash;
-	char		*str;
+	int				l;
+	long long		hash, r_with_power;
+	char			*str;
+	const long long r = 31;
+	const long long	M = 1234567891;
 
 	scanf("%d", &l);
 	str = (char *) malloc (sizeof(char) * (l + 1));
 	scanf("%s", str);
 	hash = 0;
+	r_with_power = 1;
 	for (int i = 0; *(str + i) != '\0'; ++i)
-		hash += ((int) (*(str + i) - 'a' + 1)) * ft_power(31, i);
-	hash %= 1234567891;
+	{
+		hash += ((long long) (*(str + i) - 'a' + 1)) * r_with_power;
+		hash %= M;
+		r_with_power *= r;
+		r_with_power %= M;
+	}
 	printf("%lld", hash);
 	free(str);
 	return (0);
