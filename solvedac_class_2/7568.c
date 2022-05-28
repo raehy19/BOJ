7e099748d020a3dcca103f7f228a94e7f21c247a
raehy19 <69339099+raehy19@users.noreply.github.com>
2022-05-28 17:25:07 +0900

    7568

--- solvedac_class_2/7568.c
+++ solvedac_class_2/7568.c
@@ -5,28 +5,12 @@ typedef struct s_data
 {
 	int weight;
 	int	height;
-	int idx;
-	int rank;
 } t_data;
 
-int	ft_compare_idx(const void *a, const void *b)
+int	ft_is_bigger(t_data *a, t_data *b)
 {
-	if (((t_data *) a)->idx > ((t_data *) b)->idx)
+	if ((a->weight > b->weight) && (a->height > b->height))
 		return (1);
-	else if (((t_data *) a)->idx < ((t_data *) b)->idx)
-		return (-1);
-	else
-		return (0);
-}
-
-int	ft_compare_weight_height(const void *a, const void *b)
-{
-	if ((((t_data *) a)->weight < ((t_data *) b)->weight)
-		&& (((t_data *) a)->height < ((t_data *) b)->height))
-		return (1);
-	else if ((((t_data *) a)->weight > ((t_data *) b)->weight)
-		&& (((t_data *) a)->height > ((t_data *) b)->height))
-		return (-1);
 	else
 		return (0);
 }
@@ -39,22 +23,15 @@ int main()
 	scanf("%d", &n);
 	datas = (t_data *) malloc(sizeof (t_data) * n);
 	for (int i = 0; i < n; ++i)
-	{
 		scanf("%d %d", &(datas + i)->weight, &(datas + i)->height);
-		(datas + i)->idx = i;
-	}
-	qsort(datas, n, sizeof(t_data), ft_compare_weight_height);
-	temp = 1;
-	datas->rank = 1;
-	for (int i = 1; i < n; ++i)
-	{
-		if (ft_compare_weight_height((datas + i - 1), (datas + i)) == -1)
-			temp = i + 1;
-		(datas + i)->rank = temp;
-	}
-	qsort(datas, n, sizeof(t_data), ft_compare_idx);
 	for (int i = 0; i < n; ++i)
-		printf("%d ", (datas + i)->rank);
+	{
+		temp = 1;
+		for (int j = 0; j < n; ++j)
+			if (ft_is_bigger((datas + j), (datas + i)) == 1)
+				++temp;
+		printf("%d ", temp);
+	}
 	free(datas);
 	return (0);
 }
