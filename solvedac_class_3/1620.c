cf0860f40346e97d28b26b43160356c9c738e136
raehy19 <69339099+raehy19@users.noreply.github.com>
2022-06-06 23:39:17 +0900

    1620 : memory overflow

--- solvedac_class_3/1620.c
+++ solvedac_class_3/1620.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
 int	ft_atoi(char *str)
@@ -16,18 +15,39 @@ int	ft_atoi(char *str)
 	return (result);
 }
 
+long long	ft_hash(char *str)
+{
+	long long	hash, r_with_power;
+	const long long r = 59;
+	const long long	M = 199996169;
+
+	hash = 0;
+	r_with_power = 1;
+	for (int i = 0; *(str + i) != '\0'; ++i)
+	{
+		hash += ((long long) (*(str + i) - 64)) * r_with_power;
+		hash %= M;
+		r_with_power *= r;
+		r_with_power %= M;
+	}
+	return (hash);
+}
+
 int main()
 {
 	int 	n, m;
 	char	**pokemons;
+	int		*hash_map;
 	char	question[21];
 
 	scanf("%d %d", &n, &m);
 	pokemons = (char **) malloc(sizeof(char *) * n);
+	hash_map = (int *) malloc(sizeof(int) * 199996169);
 	for (int i = 0; i < n; ++i)
 	{
 		*(pokemons + i) = (char *) malloc(sizeof(char) * 21);
 		scanf("%s", *(pokemons + i));
+		*(hash_map + ft_hash(*(pokemons + i))) = i + 1;
 	}
 	for (int i = 0; i < m; ++i)
 	{
@@ -35,19 +55,11 @@ int main()
 		if (*(question) >= '0' && *(question) <= '9')
 			printf("%s\n", *(pokemons + ft_atoi(question) - 1));
 		else
-		{
-			for (int j = 0; j < n; ++j)
-			{
-				if (strcmp(question, *(pokemons + j)) == 0)
-				{
-					printf("%d\n", j + 1);
-					break ;
-				}
-			}
-		}
+			printf("%d\n", *(hash_map + ft_hash(question)));
 	}
 	for (int i = 0; i < n; ++i)
 		free(*(pokemons + i));
 	free(pokemons);
+	free(hash_map);
 	return (0);
 }
