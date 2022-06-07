7027e60c1845c67b29a3aa34c0f13ba68a90c76b
raehy19 <69339099+raehy19@users.noreply.github.com>
2022-06-07 21:54:51 +0900

    11047 : wrong

---
+++ solvedac_class_3/11047.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int	n, k, coin_count;
+	int *coins;
+
+	scanf("%d %d", &n, &k);
+	coins = (int *) malloc(sizeof(int) * n);
+	for (int i = 0; i < n; ++i)
+		scanf("%d", coins + i);
+	coin_count = 0;
+	for (int i = n; i > 0; --i)
+	{
+		coin_count += k / *(coins + i);
+		k %= *(coins + i);
+	}
+	printf("%d", coin_count);
+	free(coins);
+	return (0);
+}
