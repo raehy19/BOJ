da39d182e1b87d689eb09603189aff93f395bf66
raehy19 <69339099+raehy19@users.noreply.github.com>
2022-06-10 23:28:54 +0900

    1003

--- solvedac_class_3/1003.c
+++ solvedac_class_3/1003.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
-int	ft_fibonacci(int n)
-{
-	if (n < 2)
-		return (n);
-	else
-		return (ft_fibonacci(n - 1) + ft_fibonacci(n - 2));
-}
-
 int main()
 {
-	int		num_of_case, input_num;
+	int					num_of_case, temp;
+	unsigned long long	fibo_arr[41];
 
+	*(fibo_arr + 0) = 0;
+	*(fibo_arr + 1) = 1;
+	for (int i = 2; i < 41; ++i)
+		*(fibo_arr + i) = *(fibo_arr + i - 1) + *(fibo_arr + i - 2);
 	scanf("%d", &num_of_case);
 	for (int i = 0; i < num_of_case; ++i)
 	{
-		scanf("%d", &input_num);
-		if (input_num == 0)
+		scanf("%d", &temp);
+		if (temp == 0)
 			printf("1 0\n");
 		else
-			printf("%d %d\n", ft_fibonacci(input_num - 1), ft_fibonacci(input_num));
+			printf("%lld %lld\n", *(fibo_arr + temp - 1), *(fibo_arr + temp));
 	}
 	return (0);
 }
