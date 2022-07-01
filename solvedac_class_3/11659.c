51aa583f71a7147e822c8a091b0e3e9c249e72c3
raehy19 <69339099+raehy19@users.noreply.github.com>
2022-07-01 23:30:06 +0900

    11659 : refactor

--- solvedac_class_3/11659.c
+++ solvedac_class_3/11659.c
@@ -3,23 +3,22 @@
 
 int main()
 {
-	int	num_of_nums, num_of_case, start, end;
-	int	*nums, *sums;
+	int	num_of_nums, num_of_case, input_num, start, end;
+	int	*sums;
 
 	scanf("%d %d", &num_of_nums, &num_of_case);
-	nums = (int *) malloc(sizeof(int) * num_of_nums);
 	sums = (int *) malloc(sizeof(int) * (num_of_nums + 1));
-	for (int i = 0; i < num_of_nums; ++i)
-		scanf("%d", nums + i);
 	*(sums) = 0;
 	for (int i = 1; i < num_of_nums + 1; ++i)
-		*(sums + i) = *(sums + i - 1) + *(nums + i - 1);
+	{
+		scanf("%d", &input_num);
+		*(sums + i) = *(sums + i - 1) + input_num;
+	}
 	for (int i = 0; i < num_of_case; ++i)
 	{
 		scanf("%d %d", &start, &end);
 		printf("%d\n", *(sums + end) - *(sums + start - 1));
 	}
-	free(nums);
 	free(sums);
 	return (0);
 }
