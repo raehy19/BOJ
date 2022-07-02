f25d0e95a04303d2e1d749a2046357463a3eb27b
raehy19 <69339099+raehy19@users.noreply.github.com>
2022-07-02 23:36:27 +0900

    11727

---
+++ solvedac_class_3/11727.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int			n;
+	int			*dp;
+	const int	mod = 10007;
+
+	scanf("%d", &n);
+	dp = (int *) malloc(sizeof(int) * n);
+	*(dp + 0) = 1;
+	if (n > 1)
+		*(dp + 1) = 3;
+	for (int i = 2; i < n; ++i)
+		*(dp + i) = (*(dp + i - 1) + *(dp + i - 2) * 2) % mod;
+	printf("%d", *(dp + n - 1));
+	free(dp);
+	return (0);
+}
